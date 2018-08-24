553f6b3d30c07573b3324089d81edd2f55b3bdcb
jun.yi <jun.yi@westalgo.com>
2018-08-24 20:34:06 +0800

    Enable mouse right paste feature in putty

--- init.el
+++ init.el
@@ -458,6 +458,7 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
+  (xterm-mouse-mode -1)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
