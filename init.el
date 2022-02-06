b61df99f5e4e03b7105ce1cac29e0bcbe76a4226
Nutall <nutall@outlook.com>
2022-02-06 23:45:49 +0800

    Add treemacs layer
    Signed-off-by: Nutall <nutall@outlook.com>

--- init.el
+++ init.el
@@ -92,7 +92,7 @@ This function should only modify configuration layer settings."
      ;; better-defaults
      ;; spell-checking
      ;; version-control
-     )
+     treemacs)
 
    ;; List of additional packages that will be installed without being wrapped
    ;; in a layer (generally the packages are installed only and should still be
